e431a1424fbbca108a012cf4be8024dac7c3e349
lukas <lukas.schlicher04@gmail.com>
2026-06-01 17:02:58 +0200

    fixed image hover logic

--- src/components/ArchitectureSection.jsx
+++ src/components/ArchitectureSection.jsx
@@ -11,7 +11,6 @@ const ArchitectureSection = ({ onOpenModal }) => (
                 <img
                     src="https://kubernetes.io/images/docs/kubernetes-cluster-architecture.svg"
                     alt="Offizielle Architektur"
-                    {/* FIXED: Wrapped in an arrow function */}
                     onClick={() => onOpenModal("https://kubernetes.io/images/docs/kubernetes-cluster-architecture.svg")}
                     style={{
                         maxWidth: '100%',
@@ -36,7 +35,6 @@ const ArchitectureSection = ({ onOpenModal }) => (
                 <p style={{ marginBottom: '1.5rem' }}>Visualisierung der konkreten Implementierung inkl. Cloudflare, HPA und git-sync:</p>
                 <img
                     src="https://www.plantuml.com/plantuml/proxy?cache=no&src=https://raw.githubusercontent.com/Lukas204/K8SPresentationWebsite/main/architecture.puml"
-                    {/* FIXED: Wrapped in an arrow function */}
                     onClick={() => onOpenModal("https://www.plantuml.com/plantuml/proxy?cache=no&src=https://raw.githubusercontent.com/Lukas204/K8SPresentationWebsite/main/architecture.puml")}
                     alt="Projekt Architektur"
                     style={{ maxWidth: '100%', height: 'auto', borderRadius: '8px', boxShadow: '0 4px 10px rgba(0,0,0,0.1)', cursor: 'zoom-in'}}
